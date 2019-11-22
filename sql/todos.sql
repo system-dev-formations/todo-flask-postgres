2a9c7b83945bac637c3e109d4ea6799b3f4c22e2
plb-formation <dockerlite@gmail.com>
2019-11-22 18:21:13 +0100

    added create database tododb and 1 row

--- sql/todos.sql
+++ sql/todos.sql
@@ -1,71 +1,19 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 172.17.0.2
--- Generation Time: Nov 20, 2019 at 06:08 AM
--- Server version: 5.6.38-log
--- PHP Version: 7.2.19
+-- Table: public.todos
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- DROP TABLE public.todos;
 
+create table todos
+(
+    todo_id  serial      not null
+        constraint firstkey
+            primary key,
+    title    varchar(60) not null,
+    text     varchar(80) not null,
+    done     varchar(50) not null,
+    pub_date date        not null
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+alter table todos
+    owner to postgres;
 
---
--- Database: `tododb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `todos`
---
-USE tododb;
-
-CREATE TABLE `todos` (
-  `todo_id` bigint(20) UNSIGNED NOT NULL,
-  `title` varchar(60) COLLATE latin1_bin NOT NULL,
-  `text` varchar(80) COLLATE latin1_bin NOT NULL,
-  `done` varchar(50) COLLATE latin1_bin NOT NULL,
-  `pub_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
-
---
--- Dumping data for table `todos`
---
-
-INSERT INTO `todos` (`todo_id`, `title`, `text`, `done`, `pub_date`) VALUES
-(1, 'test', 'test', '0', '2019-11-20'),
-(2, 'qwerty', 'yyy', '0', '2019-11-20');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `todos`
---
-ALTER TABLE `todos`
-  ADD UNIQUE KEY `todo_id` (`todo_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `todos`
---
-ALTER TABLE `todos`
-  MODIFY `todo_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO public.todos (todo_id, title, text, done, pub_date) VALUES (1, 'CrunchyDevops', 'test', 'false', '2019-11-22');
